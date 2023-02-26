ad50578651cdd9b8844ede5c144a04be052d93d7
Fabrício Barbosa Viegas <incy8498@gmail.com>
2023-02-25 23:45:58 -0300

    Update audio.py

--- robots/audio.py
+++ robots/audio.py
@@ -2,15 +2,33 @@ from gtts import gTTS
 import os
 from langdetect import detect
 from utils import get_file_number
+from unidecode import unidecode
+from pydub import AudioSegment
 
 
 
 def generate_audio(text, theme):
     audio = gTTS(text=text, lang=detect(text), slow=False)
 
-    path = os.path.join("projects/audios", theme.replace(' ', '-'))
+    path = os.path.join("projects/audios", unidecode(theme.replace(' ', '-')))
 
     if not os.path.exists(path):
         os.mkdir(path)
 
     audio.save(f"{path}/{get_file_number(path)}.mp3")
+
+
+
+def merge_audios(path):
+    audios = []
+
+    for audio in os.listdir(path):
+        if audio.endswith(".mp3"):
+            audios.append(AudioSegment.from_mp3(f"{path}/{audio}"))
+
+    combined = audios[0]
+
+    for i in audios[1:]:
+        combined += i
+
+    combined.export(f"{path}/final.mp3", format="mp3")
