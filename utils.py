38963c312e2dab6829bf00ba53620f9a6ecb577a
Fabrício Barbosa Viegas <incy8498@gmail.com>
2023-02-26 11:06:16 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import unidecode
+
 
 
 def get_file_number(dir_path):
@@ -14,3 +16,29 @@ def get_file_number(dir_path):
         return count + 1
     except:
         return count
+    
+
+
+def cleanAllPaths(theme):
+    
+    try:
+        cleanPath(f"projects/images/{unidecode(theme.replace(' ', '-'))}")
+        cleanPath(f"projects/audios/{unidecode(theme.replace(' ', '-'))}")
+        cleanPath(f"projects/videos/{unidecode(theme.replace(' ', '-'))}")
+    except:
+        pass
+
+
+
+def cleanPath(path):
+
+   for filename in os.listdir(path):
+
+       file_path = os.path.join(path, filename)
+
+       try:
+           if os.path.isfile(file_path) or os.path.islink(file_path):
+               os.unlink(file_path)
+
+       except Exception as e:
+           print('Failed to delete %s. Reason: %s' % (file_path, e))
